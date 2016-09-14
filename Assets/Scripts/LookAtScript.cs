083d2944f77a04e85f7e286067786d5a6ec30dae
Jakob Kylberg <jakob.kylberg@gmail.com>
2016-09-14 18:38:00 +0200

    Implemented keyboard control of the view.

--- Assets/Scripts/LookAtScript.cs
+++ Assets/Scripts/LookAtScript.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class LookAtScript : MonoBehaviour {
 
 	public GameObject target;//the target object
+	public int rotationStep = 3;
 
-	private float speedMod = 0.50f;//a speed modifier
 	private Vector3 point;//the coord to the point where the camera looks at
 
 	void Start () {//Set up things on the start method
@@ -14,6 +15,7 @@ public class LookAtScript : MonoBehaviour {
 	}
 
 	void Update () {//makes the camera rotate around "point" coords, rotating around its Y axis, 20 degrees per second times the speed modifier
-		transform.RotateAround (point,new Vector3(0.0f,1.0f,0.0f),20 * Time.deltaTime * speedMod);
+		float changeHorizontalAxis = CrossPlatformInputManager.GetAxis("Horizontal");
+		transform.RotateAround(point, new Vector3(0.0f, changeHorizontalAxis, 0.0f), rotationStep);
 	}
 }
