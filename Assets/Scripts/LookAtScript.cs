79609d81f164aa1e602f2f3ad5dba0d2919a9f0d
Jakob Kylberg <jakob.kylberg@gmail.com>
2016-09-14 19:47:23 +0200

    Added view change in the y-direction and a configurable max y view tilt.

--- Assets/Scripts/LookAtScript.cs
+++ Assets/Scripts/LookAtScript.cs
@@ -6,6 +6,8 @@ public class LookAtScript : MonoBehaviour {
 
 	public GameObject target;//the target object
 	public int rotationStep = 3;
+	public float positiveVerticalViewLimit = 10.0f;
+	public float negativeVerticalViewLimit = -10.0f;
 
 	private Vector3 point;//the coord to the point where the camera looks at
 
@@ -16,6 +18,21 @@ public class LookAtScript : MonoBehaviour {
 
 	void Update () {//makes the camera rotate around "point" coords, rotating around its Y axis, 20 degrees per second times the speed modifier
 		float changeHorizontalAxis = CrossPlatformInputManager.GetAxis("Horizontal");
+		float changeVerticalAxis = -CrossPlatformInputManager.GetAxis("Vertical");
+		//point.Set (point.x, point.y - changeVerticalAxis, point.z);
+		point.Set (point.x, point.y + getLimitedVerticalChange(changeVerticalAxis), point.z);
+		transform.LookAt(point);
 		transform.RotateAround(point, new Vector3(0.0f, changeHorizontalAxis, 0.0f), rotationStep);
 	}
+
+	float getLimitedVerticalChange(float rawChange) {
+		if (point.y + rawChange > positiveVerticalViewLimit) {
+			return positiveVerticalViewLimit - point.y;
+		} else if (point.y + rawChange < negativeVerticalViewLimit) {
+			return negativeVerticalViewLimit - point.y;
+		} else {
+			return rawChange;
+		}
+	}
+
 }
