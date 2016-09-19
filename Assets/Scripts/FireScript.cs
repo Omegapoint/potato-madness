fa008aae84c76eeaea7aaa39fb94c9cec6655326
Fredrik Stålnacke <fredrik.stalnacke@omegapoint.se>
2016-09-19 19:59:07 +0200

    Added more fire-sounds and corrected the fire-script to aim out of the tube.

--- Assets/Scripts/FireScript.cs
+++ Assets/Scripts/FireScript.cs
@@ -1,33 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class FireScript : MonoBehaviour {
 
 	public GameObject firetube;
 	public GameObject potatoPrefab;
+	public AudioClip[] fireSoundClips;
+	public int shotForce = 3000;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Fire1")) {
-			Debug.Log ("Tänt var det här!");
-			Debug.Log (Input.mousePosition);
-			//GameObject potato = GameObject.Find("Potato");
+			Debug.Log ("rotation: " + firetube.transform.rotation);
+			Debug.Log ("eulerAngles: " + firetube.transform.eulerAngles);
+			GameObject newPotato = (GameObject)Instantiate (potatoPrefab, firetube.transform.position, 
+				Quaternion.Euler (firetube.transform.eulerAngles.x, firetube.transform.eulerAngles.y, firetube.transform.eulerAngles.z));
+			newPotato.transform.eulerAngles = firetube.transform.eulerAngles;
 
-			Debug.Log (firetube.transform.position);
-			GameObject newPotato = (GameObject)Instantiate(potatoPrefab, firetube.transform.position, firetube.transform.rotation);
-			newPotato.name = "fire";
+			newPotato.name = "potatofire";
 			Rigidbody potatoRigidBody = newPotato.GetComponent<Rigidbody> ();
-			potatoRigidBody.velocity = Vector3.zero;
-			//potatoRigidBody.isKinematic = true;
-			potatoRigidBody.AddForce(transform.forward * 30000);
+			potatoRigidBody.AddForce (potatoRigidBody.transform.up * shotForce);
 
-			AudioSource audio = GetComponent<AudioSource>();
-			audio.Play();
+			PlayFireSound ();
 		} 
+	}
+
+	private void PlayFireSound() {
+		AudioSource audio = GetComponent<AudioSource>();
+		audio.clip = fireSoundClips[Random.Range(0, fireSoundClips.Length)];
+		audio.Play();
 	}
 }
