bfdd12bca744f49b931a223552bd7e592791fd31
Fredrik Stålnacke <fredrik.stalnacke@omegapoint.se>
2016-09-20 07:04:02 +0200

    Added a embryo for a game manager for handling number of bricks left.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public Text scoreText;
+
+	private int totalNumberOfBricks;
+	private int currentNumberOfBricks;
+
+	// Use this for initialization
+	void Start () {
+		totalNumberOfBricks = GameObject.FindGameObjectsWithTag ("block").Length;
+		currentNumberOfBricks = totalNumberOfBricks;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentNumberOfBricks = GameObject.FindGameObjectsWithTag ("block").Length;
+		scoreText.text = currentNumberOfBricks + " av " + totalNumberOfBricks;
+	}
+}
