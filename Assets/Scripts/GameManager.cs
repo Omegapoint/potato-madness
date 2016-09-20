856693fd61ee776943834c86ed98c06e7f903064
Fredrik Stålnacke <fredrik.stalnacke@omegapoint.se>
2016-09-20 08:26:55 +0200

    Changed text to display the number of blocks that has been taken down instead of the number that are left.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,6 +18,6 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		currentNumberOfBricks = GameObject.FindGameObjectsWithTag ("block").Length;
-		scoreText.text = currentNumberOfBricks + " av " + totalNumberOfBricks;
+		scoreText.text = (totalNumberOfBricks - currentNumberOfBricks) + " av " + totalNumberOfBricks;
 	}
 }
