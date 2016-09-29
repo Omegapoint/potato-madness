cdb95d9dde2344fdab7643bc88f67768144d6cac
Jakob Kylberg <jakob.kylberg@gmail.com>
2016-09-29 20:30:03 +0200

    First cut of an event system that handles shots fired and targets left.

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class ScoreText : MonoBehaviour {
+
+	void OnEnable ()
+	{
+		EventManager.StartListening ("targetKnockedDown", TargetKnockedDown);
+	}
+
+	void OnDisable ()
+	{
+		EventManager.StopListening ("targetKnockedDown", TargetKnockedDown);
+	}
+
+	void TargetKnockedDown ()
+	{
+		Debug.Log ("TargetKnockedDown Function was called!");
+	}
+
+}
---
+++ Assets/Scripts/ShotsLeft.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShotsLeft : MonoBehaviour {
+
+	private Text scoreText;
+
+	void Start() {
+		scoreText = gameObject.GetComponent<Text>();
+		UpdateText ();
+	}
+
+	void OnEnable ()
+	{
+		EventManager.StartListening ("shotFired", ShotFired);
+	}
+
+	void OnDisable ()
+	{
+		EventManager.StopListening ("shotFired", ShotFired);
+	}
+
+	void UpdateText() {
+		scoreText.text = GameManager.gm.currentLevel.ShotsLeft() + " shots left";
+	}
+
+	void ShotFired ()
+	{
+		Text scoreText = gameObject.GetComponent<Text>();
+		Debug.Log ("ShotFired Function was called!");
+		UpdateText ();
+	}
+}
