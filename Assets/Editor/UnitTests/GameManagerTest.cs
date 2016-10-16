8162df61def9a5979d100b61d5f4f6fe18396c83
Jakob Kylberg <jakob.kylberg@gmail.com>
2016-10-16 14:17:00 +0200

    Added unit test example.

---
+++ Assets/Editor/UnitTests/GameManagerTest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace UnityTest
+{
+	[TestFixture]
+	[Category("GameManager Tests")]
+	internal class GameManagerTest
+	{
+		[Test]
+		[Category("Level Handling")]
+		public void ShouldDetermineThatWeaAreAtTheFinalLevelWithOnlyOneLevelInTotal()
+		{
+			// given
+			GameObject go = new GameObject();
+			go.AddComponent<GameManager>();
+
+			GameLevel[] gameLevels = new GameLevel[1] {GameLevel.CreateInstance<GameLevel>()};
+
+			GameManager gameManager = go.GetComponent<GameManager> ();
+			gameManager.levels = gameLevels;
+			// when
+			bool isFinalLevel = gameManager.IsFinalLevel();
+			// then
+			Assert.IsTrue(isFinalLevel);
+		}
+			
+	}
+}
